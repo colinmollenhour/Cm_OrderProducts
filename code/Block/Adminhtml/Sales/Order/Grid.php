48880b443803cc82a43aec5727faff3c8ede9566
Colin Mollenhour <colin@mollenhour.com>
2015-02-16 21:25:19 -0500

    Fix event to run on export.

--- code/Block/Adminhtml/Sales/Order/Grid.php
+++ code/Block/Adminhtml/Sales/Order/Grid.php
@@ -14,7 +14,6 @@ class Cm_OrderProducts_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_B
      * MUST override setCollection rather than _prepareCollection to get filtering and paging both working
      *
      * @param Mage_Sales_Model_Mysql4_Order_Grid_Collection $collection
-     * @return
      */
     public function setCollection($collection)
     {
@@ -45,69 +44,81 @@ class Cm_OrderProducts_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_B
      * - Wrapping the main query as a subquery is too complex
      * - We want to show all order items for orders that matched filter by sku/name
      *
-     * @return mixed
+     * @return $this
      */
     protected function _prepareCollection()
     {
         parent::_prepareCollection();
-        if ( ! Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN) || $this->_isExport) return;
 
-        $orderIds = array();
-        $orderCollection = $this->getCollection(); /** @var $orderCollection Mage_Sales_Model_Mysql4_Order_Grid_Collection */
-        foreach ($orderCollection as $order) {
-            $orderIds[] = $order->getEntityId();
-        }
-        $conn = Mage::getSingleton('core/resource')->getConnection('read'); /* @var $conn Zend_Db_Adapter_Pdo_Abstract */
-
-        // Increase max length of group concat fields for long product names
-        $conn->exec('SET SESSION group_concat_max_len = 4096;');
-
-        $itemsCollection = new Varien_Data_Collection_Db($conn);
-        $itemsCollection->getSelect()
-            ->from(array('sales/order_item' => $orderCollection->getTable('sales/order_item')), array(
-                'order_id',
-                'skus'  => new Zend_Db_Expr('group_concat(`sales/order_item`.sku SEPARATOR " ^ ")'),
-                'qtys'  => new Zend_Db_Expr('group_concat(`sales/order_item`.qty_ordered SEPARATOR " ^ ")'),
-                'names' => new Zend_Db_Expr('group_concat(`sales/order_item`.name SEPARATOR " ^ ")'),
-            ))
-            ->where('order_id IN (?)', $orderIds)
-            ->group('order_id');
-        foreach ($itemsCollection as $object)
+        if (Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN) && ! $this->_isExport)
         {
-            $order = $orderCollection->getItemById($object->getOrderId());
-            $order->setSkus($object->getSkus());
-            $order->setQtys($object->getQtys());
-            $order->setNames($object->getNames());
+            $orderIds = array();
+            $orderCollection = $this->getCollection(); /** @var $orderCollection Mage_Sales_Model_Mysql4_Order_Grid_Collection */
+            foreach ($orderCollection as $order) {
+                $orderIds[] = $order->getEntityId();
+            }
+            $conn = Mage::getSingleton('core/resource')->getConnection('read'); /* @var $conn Zend_Db_Adapter_Pdo_Abstract */
+
+            // Increase max length of group concat fields for long product names
+            $conn->exec('SET SESSION group_concat_max_len = 4096;');
+
+            $itemsCollection = new Varien_Data_Collection_Db($conn);
+            $itemsCollection->getSelect()
+                            ->from(array('sales/order_item' => $orderCollection->getTable('sales/order_item')), array(
+                                'order_id',
+                                'skus'  => new Zend_Db_Expr('group_concat(`sales/order_item`.sku SEPARATOR " ^ ")'),
+                                'qtys'  => new Zend_Db_Expr('group_concat(`sales/order_item`.qty_ordered SEPARATOR " ^ ")'),
+                                'names' => new Zend_Db_Expr('group_concat(`sales/order_item`.name SEPARATOR " ^ ")'),
+                            ))
+                            ->where('order_id IN (?)', $orderIds)
+                            ->group('order_id');
+            foreach ($itemsCollection as $object)
+            {
+                $order = $orderCollection->getItemById($object->getOrderId());
+                $order->setSkus($object->getSkus());
+                $order->setQtys($object->getQtys());
+                $order->setNames($object->getNames());
+            }
+
         }
 
         Mage::app()->dispatchEvent('cm_orderproducts_sales_order_grid_prepareCollection', ['block' => $this]);
+
+        return $this;
     }
 
+    /**
+     * @return $this
+     */
     protected function _prepareColumns()
     {
         parent::_prepareColumns();
-        if( ! Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN) || $this->_isExport) return;
 
-        // Specify table to fix ambiguous column errors
-        foreach($this->getColumns() as $column) {
-            if($column->getIndex()) {
-                $column->setFilterIndex('`main_table`.'.$column->getIndex());
+        if (Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN) && ! $this->_isExport)
+        {
+            // Specify table to fix ambiguous column errors
+            foreach($this->getColumns() as $column) {
+                if($column->getIndex()) {
+                    $column->setFilterIndex('`main_table`.'.$column->getIndex());
+                }
             }
+
+            $this->addColumnAfter('skus', array(
+                'header'    => Mage::helper('sales')->__('Products Ordered (%s)', Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN)),
+                'index'     => 'skus',
+                'type'      => 'text',
+                'filter_index' => '`sales/order_item`.'. Mage::getStoreConfig(self::XML_PATH_FILTER_COLUMN),
+                'sortable'  => FALSE,
+                'renderer'  => 'Cm_OrderProducts_Block_Adminhtml_Sales_Order_Grid_Renderer_Products',
+                'render_column' =>  Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN),
+            ), 'shipping_name');
+
+            $this->sortColumnsByOrder();
         }
 
-        $this->addColumnAfter('skus', array(
-            'header'    => Mage::helper('sales')->__('Products Ordered (%s)', Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN)),
-            'index'     => 'skus',
-            'type'      => 'text',
-            'filter_index' => '`sales/order_item`.'. Mage::getStoreConfig(self::XML_PATH_FILTER_COLUMN),
-            'sortable'  => FALSE,
-            'renderer'  => 'Cm_OrderProducts_Block_Adminhtml_Sales_Order_Grid_Renderer_Products',
-            'render_column' =>  Mage::getStoreConfig(self::XML_PATH_RENDER_COLUMN),
-        ), 'shipping_name');
-
-        $this->sortColumnsByOrder();
-
         Mage::app()->dispatchEvent('cm_orderproducts_sales_order_grid_prepareColumns', ['block' => $this]);
+
+        return $this;
     }
 
 }
